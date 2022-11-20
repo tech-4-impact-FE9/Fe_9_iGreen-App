c7898f759cae30750a86ef8fd8a3d9fe8f07436b
bismayanuario <yanuariobisma23@gmail.com>
2022-11-20 12:26:45 +0700

    Add Login and Register Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./component/Home/Home"
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Footer from "./component/Footer/Footer"
+import Login from "./component/Login/Login"
+import Register from "./component/Register/Register"
 
 
 //npm install react-router-dom@5
@@ -15,9 +17,16 @@ function App() {
         <Navbar />
 
         <Switch>
-          <Route path='/' compoent={Home} exact>
+          <Route path='/' component={Home} exact>
             <Home />
           </Route>
+          <Route path='/Login' component={Login} exact>
+            <Login/>
+          </Route>
+          <Route path='/Register' component={Register} exact>
+            <Register/>
+          </Route>
+          
           
         </Switch>
         
